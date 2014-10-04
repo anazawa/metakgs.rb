50503caee3ab438b6378cfca5cd4e998a5a11904
Ryo Anazawa <anazawa@cpan.org>
2014-10-04 23:53:34 +0900

    add @user_agent, @api_endopoint and @http

--- lib/metakgs/client.rb
+++ lib/metakgs/client.rb
@@ -1,22 +1,37 @@
-require 'metakgs/archives'
-require 'metakgs/client/archives'
-require 'net/http'
 require 'json'
+require 'metakgs/client/archives'
+require 'metakgs/version'
+require 'net/http'
 require 'uri'
 
 module MetaKGS
   class Client
+
     include MetaKGS::Client::Archives
 
-    def api_endpoint
-      "http://metakgs.org/api"
+    attr_reader :api_endpoint, :http
+    attr_accessor :user_agent
+
+    def initialize( args = {} )
+      @user_agent = args[:user_agent] || "MetaKGS Ruby Gem #{MetaKGS::VERSION}"
+      @api_endpoint = URI( args[:api_endpoint] || "http://metakgs.org/api" )
+      @http = Net::HTTP.new( api_endpoint.host, api_endpoint.port )
     end
 
-    def get( url )
-      url = api_endpoint + "/#{url}" unless url.match(/^https?:\/\//)
-      response = Net::HTTP.get_response( URI.parse(url) )
+    def get( path )
+      response = do_get path =~ /^https?:\/\// ? URI(path) : uri_for path
       response.value unless Net::HTTPSuccess === response
       JSON.parse( response.body )
+    end
+
+    def do_get( url )
+      http.get(url.path, {
+        'User-Agent' => user_agent,
+      })
+    end
+
+    def uri_for( path )
+      URI( File.join(api_endpoint.to_s, path) )
     end
 
   end
--- lib/metakgs/client/archives.rb
+++ lib/metakgs/client/archives.rb
@@ -3,7 +3,17 @@ module MetaKGS
     module Archives
 
       def get_archives( query = {} )
-        do_get_archives MetaKGS::Archives.build_path( query )
+        now = Time.now.gmtime
+        user = query[:user]
+        year = query[:year] || now.year
+        month = query[:month] || now.mon
+
+        raise ":user is required" unless query.has_key?(:user)
+        raise ":user is invalid" unless user.is_a? String and user.match(/^[a-zA-Z][a-zA-Z0-9]{0,9}$/)
+        raise ":year is invalid" unless year.is_a? Integer and year >= 2000
+        raise ":month is invalid" unless month.is_a? Integer and month.between?(1, 12)
+
+        do_get_archives "archives/#{user}/#{year}/#{month}"
       end
 
       def do_get_archives( url )
